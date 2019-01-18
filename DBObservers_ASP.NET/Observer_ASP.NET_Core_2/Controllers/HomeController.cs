11a60b6b7c78461ab338ff359da548418ddd723c
Danil <DukhovnikovDanil@yandex.ru>
2019-01-18 04:42:21 +0300

    Step3
    Сортировка

---
+++ DBObservers_ASP.NET/Observer_ASP.NET_Core_2/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Observer_ASP.NET_Core_2.Models;
+
+namespace Observer_ASP.NET_Core_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        UsersContext db;
+        public HomeController(UsersContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<IActionResult> Index(SortState sortOrder = SortState.NameAsc)
+        {
+            IQueryable<User> users = db.Users.Include(x => x.Company);
+
+            ViewData["NameSort"] = sortOrder == SortState.NameAsc ? SortState.NameDesc : SortState.NameAsc;
+            ViewData["AgeSort"] = sortOrder == SortState.AgeAsc ? SortState.AgeDesc : SortState.AgeAsc;
+            ViewData["CompSort"] = sortOrder == SortState.CompanyAsc ? SortState.CompanyDesc : SortState.CompanyAsc;
+
+            switch (sortOrder)
+            {
+                case SortState.NameDesc:
+                    users = users.OrderByDescending(s => s.Name);
+                    break;
+                case SortState.AgeAsc:
+                    users = users.OrderBy(s => s.Age);
+                    break;
+                case SortState.AgeDesc:
+                    users = users.OrderByDescending(s => s.Age);
+                    break;
+                case SortState.CompanyAsc:
+                    users = users.OrderBy(s => s.Company.Name);
+                    break;
+                case SortState.CompanyDesc:
+                    users = users.OrderByDescending(s => s.Company.Name);
+                    break;  
+                default:
+                    users = users.OrderBy(s => s.Name);
+                    break;
+            }
+            return View(await users.AsNoTracking().ToListAsync());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DBObservers_ASP.NET/Observer_ASP.NET_Core_2/Models/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Observer_ASP.NET_Core_2.Models
+{
+    public sealed class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public UsersContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<User> Users { get; set; }
+        public Company()
+        {
+            Users = new List<User>();
+        }
+    }
+}
