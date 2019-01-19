1ff74a6608866189811c1b741f1b1b2c952a5dd6
Danil <DukhovnikovDanil@yandex.ru>
2019-01-19 22:19:48 +0300

    Редактирование и вывод

--- DBObservers_ASP.NET/Observer_ASP.NET_Core_Current/Controllers/HomeController.cs
+++ DBObservers_ASP.NET/Observer_ASP.NET_Core_Current/Controllers/HomeController.cs
@@ -41,5 +41,64 @@ namespace Observer_ASP.NET_Core_Current.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id != null)
+            {
+                Equipment equipment = await db.Equipments.FirstOrDefaultAsync(p => p.Id == id);
+                if (equipment != null)
+                    return View(equipment);
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id != null)
+            {
+                Equipment equipment = await db.Equipments.FirstOrDefaultAsync(p => p.Id == id);
+                if (equipment != null)
+                    return View(equipment);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Equipment equipment)
+        {
+            db.Equipments.Update(equipment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(Guid? id)
+        {
+            if (id != null)
+            {
+                Equipment equipment = await db.Equipments.FirstOrDefaultAsync(p => p.Id == id);
+                if (equipment != null)
+                    return View(equipment);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id != null)
+            {
+                Equipment equipment = await db.Equipments.FirstOrDefaultAsync(p => p.Id == id);
+                if (equipment != null)
+                {
+                    db.Equipments.Remove(equipment);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
+        }
     }
 }
---
+++ DBObservers_ASP.NET/Observer_ASP.NET_Core_Current/Models/ESortState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Observer_ASP.NET_Core_Current.Models
+{
+    public enum SortState
+    {
+        NameAsc,    // по имени по возрастанию
+        NameDesc,   // по имени по убыванию
+        MaximumPressureForceAsc, // по максимальная сила давления по возрастанию
+        MaximumPressureForceDesc,    // по максимальная сила давления по убыванию
+        WeightAsc, // по весу по возрастанию
+        WeightyDesc // по весу по убыванию
+    }
+}
